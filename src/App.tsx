ff0450b95db5041fb3086c4e24af26360fd37f69
Edzai Nhema <nhemaed@gmail.com>
2025-12-11 12:57:19 -0400

    your message here

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,47 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <div style={{ 
-        display: "flex", 
-        justifyContent: "left", 
-        gap: "25px", 
-        marginBottom: "20px" ,
-        paddingLeft:10,
-      }}>
-        
-        <a
-			href="https://www.instagram.com/theurban_odyssey/?hl=en"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-          <Instagram size={24} color="black" />
-        </a>
-
-        
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <ShoppingBag size={24} color="black" />
-        </a>
-
-      </div>
+    <div style={{ textAlign: "center", marginTop: "00px" }}>
+		<div style={{display:'flex', flexDirection:'row', alignItems:'center', marginLeft:'10px'}}>
+			<img
+				src="/image.png"
+				alt="Urban Odyssey 1"
+				style={{
+					width: "60px",
+					height: "60px",
+					borderRadius: 30
+				}}
+			/>
+			<div style={{marginLeft:10}}>
+				<div style={{ textAlign:'left', paddingLeft:10, fontSize: "35px", marginTop: "20px" }}>
+					Urban Odyssey
+				</div>
+				<div 
+					style={{ 
+						display: "flex", 
+						justifyContent: "left", 
+						gap: "25px", 
+						marginBottom: "20px" ,
+						paddingLeft:10,
+					}}
+				>
+					
+					<a
+						href="https://www.instagram.com/theurban_odyssey/?hl=en"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Instagram size={24} color="black" />
+					</a>
+		
+		        
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<ShoppingBag size={24} color="black" />
+					</a>
+		
+				</div>
+		     </div>
+		</div>
      <div
   style={{
     display: "flex",
@@ -73,9 +91,7 @@ function App() {
       style={{ width: "300px" }}
     />
   </div>
-      <div style={{ textAlign:'left', paddingLeft:10, fontSize: "35px", marginTop: "20px" }}>
-        Urban Odyssey
-      </div>
+     
       <div style={{ 
         display: "flex", 
         justifyContent: "left", 
@@ -90,8 +106,8 @@ function App() {
 		      src="/image.png"
 		      alt="Urban Odyssey 1"
 		      style={{
-		        width: "40px",
-		        height: "40px",
+		        width: "60px",
+		        height: "60px",
 		        borderRadius: 30,
 		      }}
 		    />
@@ -105,8 +121,8 @@ function App() {
 		      src="/image_two.png"
 		      alt="Urban Odyssey 1"
 		      style={{
-		        width: "40px",
-		        height: "40px",
+		        width: "60px",
+		        height: "60px",
 		        borderRadius: 30,
 		      }}
 		    />
