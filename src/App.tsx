2f3d0d77fa21bb8a62a6935d61958ca7b3251892
Edzai Nhema <nhemaed@gmail.com>
2025-12-10 23:49:38 -0400

    Added Images

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div style={{ fontSize: "30px", textAlign: "center", marginTop: "40px" }}>
-      Urban Odyssey
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <img 
+        src="/image.png" 
+        alt="Urban Odyssey" 
+        style={{ width: "300px" }} 
+      />
+      <div style={{ fontSize: "30px", marginTop: "20px" }}>
+        Urban Odyssey
+      </div>
     </div>
   );
 }
